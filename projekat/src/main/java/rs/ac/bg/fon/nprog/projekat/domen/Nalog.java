aaa21f606170d8a489647e2192c5366aa3a578d3
IgnjatR <ignjatrajak@gmail.com>
2019-06-29 18:53:16 +0200

    Odradjena dokumentacija za domensku klasu Nalog

--- projekat/src/main/java/rs/ac/bg/fon/nprog/projekat/domen/Nalog.java
+++ projekat/src/main/java/rs/ac/bg/fon/nprog/projekat/domen/Nalog.java
@@ -1,100 +1,181 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rs.ac.bg.fon.nprog.projekat.domen;
 
 import java.io.Serializable;
 
 /**
+ * Klasa koja predstavlja klijenta radnje.
+ * 
+ * @author Ignjat Rajak
+ * @version 1.0
  *
- * @author Ignjat
  */
 public class Nalog {
-    
-    
-    private int sifraNaloga;
-    private String password;
-    private String username;
-    private Zaposleni zaposleni;
 
-    public Nalog(int sifraNaloga, String passeord, String username) {
-        this.sifraNaloga = sifraNaloga;
-        this.password = passeord;
-        this.username = username;
-    }
+	/**
+	 * Sifra naloga kao int.
+	 */
+	private int sifraNaloga;
 
-    public String getUsername() {
-        return username;
-    }
+	/**
+	 * Sifra naloga kao String.
+	 */
+	private String password;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	/**
+	 * Korisnicko ime naloga kao String.
+	 */
+	private String username;
 
-    public int getSifraNaloga() {
-        return sifraNaloga;
-    }
+	/**
+	 * Zaposleni povezan sa nalogom kao Zaposleni.
+	 */
+	private Zaposleni zaposleni;
 
-    public void setSifraNaloga(int sifraNaloga) {
-    	if(sifraNaloga<0)
-    		throw new RuntimeException("Sifra naloga mora biti veca od 0");
-        this.sifraNaloga = sifraNaloga;
-    }
+	/**
+	 * Inicijalizuje nalog i postavlja sifraNaloga, password i username na unete
+	 * vrednosti pozivanjem set metoda.
+	 * 
+	 * @param idKlijenta
+	 *            ID klijenta
+	 * @param ime
+	 *            ime klijenta
+	 * @param prezime
+	 *            prezime klijenta
+	 * 
+	 */
+	public Nalog(int sifraNaloga, String passeord, String username) {
+		this.sifraNaloga = sifraNaloga;
+		this.password = passeord;
+		this.username = username;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	/**
+	 * Metoda koja vraca username naloga .
+	 * 
+	 * @return username kao String
+	 */
+	public String getUsername() {
+		return username;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	/**
+	 * Metoda postavlja novi username naloga.
+	 * 
+	 * @param username
+	 *            nova vrednost za username
+	 * 
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    @Override
-    public String toString() {
-        return "Nalog{" + "username=" + username + '}';
-    }
+	/**
+	 * Metoda koja vraca sifru naloga naloga .
+	 * 
+	 * @return sifraNaloga kao int
+	 */
+	public int getSifraNaloga() {
+		return sifraNaloga;
+	}
 
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        return hash;
-    }
+	/**
+	 * Metoda postavlja novu sifru naloga.
+	 * 
+	 * @param sifraNaloga
+	 *            nova vrednost za sifraNaloga
+	 * @throws java.lang.RuntimeException
+	 *             ako je uneta sifraNaloga manja od 0
+	 */
+	public void setSifraNaloga(int sifraNaloga) {
+		if (sifraNaloga < 0)
+			throw new RuntimeException("Sifra naloga mora biti veca od 0");
+		this.sifraNaloga = sifraNaloga;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Nalog other = (Nalog) obj;
-        if (this.sifraNaloga != other.sifraNaloga) {
-            return false;
-        }
-        return true;
-    }
+	/**
+	 * Metoda koja vraca password naloga .
+	 * 
+	 * @return password kao String
+	 */
+	public String getPassword() {
+		return password;
+	}
 
-    public Zaposleni getZaposleni() {
-        return zaposleni;
-    }
+	/**
+	 * Metoda postavlja novi password naloga.
+	 * 
+	 * @param password
+	 *            nova vrednost za password
+	 * 
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public void setZaposleni(Zaposleni zaposleni) {
-        this.zaposleni = zaposleni;
-    }
+	@Override
+	public String toString() {
+		return "Nalog{" + "username=" + username + '}';
+	}
 
-    public Nalog(String password, String username) {
-        this.password = password;
-        this.username = username;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Nalog other = (Nalog) obj;
+		if (this.sifraNaloga != other.sifraNaloga) {
+			return false;
+		}
+		return true;
+	}
 
+	/**
+	 * Metoda koja vraca zaposlenog koji je povezan sa nalogom.
+	 * 
+	 * @return zaposleni kao Zaposleni
+	 */
+	public Zaposleni getZaposleni() {
+		return zaposleni;
+	}
+
+	/**
+	 * Metoda postavlja novog zaposlenog koji je povezan sa nalogom.
+	 * 
+	 * @param zaposleni
+	 *            nova vrednost za zaposleni
+	 * 
+	 */
+	public void setZaposleni(Zaposleni zaposleni) {
+		this.zaposleni = zaposleni;
+	}
+
+	/**
+	 * Inicijalizuje nalog i postavlja password i username na unete vrednosti
+	 * pozivanjem set metoda.
+	 * 
+	 * 
+	 * @param ime
+	 *            ime klijenta
+	 * @param prezime
+	 *            prezime klijenta
+	 * 
+	 */
+	public Nalog(String password, String username) {
+		this.password = password;
+		this.username = username;
+	}
+
+	/**
+	 * Inicijalizuje nalog
+	 */
 	public Nalog() {
 	}
-    
-    
+
 }
